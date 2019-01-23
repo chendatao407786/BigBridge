01e01cb3c7d4d3632681e13dd8d5ffcdf816ba09
CHEN Datao <chendatao1126@gmail.com>
2019-01-23 02:01:09 +0100

    Update InscriptionActivity.java

--- app/src/main/java/bigbridge/mbds/unice/fr/bigbridge/InscriptionActivity.java
+++ app/src/main/java/bigbridge/mbds/unice/fr/bigbridge/InscriptionActivity.java
@@ -17,67 +17,73 @@ import android.widget.TextView;
 
 import java.util.Calendar;
 
+import bigbridge.mbds.unice.fr.bigbridge.fragment.PersonInfoFragment;
 import bigbridge.mbds.unice.fr.bigbridge.fragment.SelectWeightFragment;
 
 public class InscriptionActivity extends AppCompatActivity {
-    int mYear;
-    int mMonth;
-    int mDay;
-    TextView mBirthday;
-    ImageView mCalendrierBtn;
-    TextView mWeight;
+//    int mYear;
+//    int mMonth;
+//    int mDay;
+//    TextView mBirthday;
+//    ImageView mCalendrierBtn;
+//    TextView mWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
-        Calendar ca = Calendar.getInstance();
-        mYear = ca.get(Calendar.YEAR);
-        mMonth = ca.get(Calendar.MONTH);
-        mDay = ca.get(Calendar.DAY_OF_MONTH);
-        mBirthday = findViewById(R.id.birthday);
-        mCalendrierBtn = findViewById(R.id.calendrier);
-        mCalendrierBtn.setOnClickListener(mOnclickListener());
-        mWeight = findViewById(R.id.weight);
+        PersonInfoFragment personInfoFragment = new PersonInfoFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.inscription,personInfoFragment);
+        transaction.commit();
+//        Calendar ca = Calendar.getInstance();
+//        mYear = ca.get(Calendar.YEAR);
+//        mMonth = ca.get(Calendar.MONTH);
+//        mDay = ca.get(Calendar.DAY_OF_MONTH);
+//        mBirthday = findViewById(R.id.birthday);
+//        mCalendrierBtn = findViewById(R.id.calendrier);
+//        mCalendrierBtn.setOnClickListener(mOnclickListener());
+//        mWeight = findViewById(R.id.weight);
     }
 
 
-    private View.OnClickListener mOnclickListener() {
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()){
-                    case R.id.calendrier:
-                        new DatePickerDialog(InscriptionActivity.this, onDateSetListener, mYear, mMonth, mDay).show();
-                        break;
-                    case R.id.weight:
-                        SelectWeightFragment selectWeightFragment = new SelectWeightFragment();
-                        selectWeightFragment.show(getSupportFragmentManager(),"dialog");
-                        break;
-                }
-
-            }
-        };
-        return listener;
-    }
-
-    private DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
-        @Override
-        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-            mYear = year;
-            mMonth = month;
-            mDay = dayOfMonth;
-            String date;
-            String sMonth = String.valueOf(month + 1);
-            String sDay = String.valueOf(dayOfMonth);
-            if (mMonth + 1 < 10) {
-                sMonth = "0" + sMonth;
-            }
-            if (mDay < 10) {
-                sDay = 0 + sDay;
-            }
-            date = new StringBuffer().append(sDay).append("/").append(sMonth).append("/").append(mYear).toString();
-            mBirthday.setText(date);
-        }
-    };
+//    private View.OnClickListener mOnclickListener() {
+//        View.OnClickListener listener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                switch (v.getId()){
+//                    case R.id.calendrier:
+//                        new DatePickerDialog(InscriptionActivity.this, onDateSetListener, mYear, mMonth, mDay).show();
+//                        break;
+//                    case R.id.weight:
+//                        SelectWeightFragment selectWeightFragment = new SelectWeightFragment();
+//                        selectWeightFragment.show(getSupportFragmentManager(),"dialog");
+//                        break;
+//                }
+//
+//            }
+//        };
+//        return listener;
+//    }
+//
+//    private DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+//        @Override
+//        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//            mYear = year;
+//            mMonth = month;
+//            mDay = dayOfMonth;
+//            String date;
+//            String sMonth = String.valueOf(month + 1);
+//            String sDay = String.valueOf(dayOfMonth);
+//            if (mMonth + 1 < 10) {
+//                sMonth = "0" + sMonth;
+//            }
+//            if (mDay < 10) {
+//                sDay = 0 + sDay;
+//            }
+//            date = new StringBuffer().append(sDay).append("/").append(sMonth).append("/").append(mYear).toString();
+//            mBirthday.setText(date);
+//        }
+//    };
 }
