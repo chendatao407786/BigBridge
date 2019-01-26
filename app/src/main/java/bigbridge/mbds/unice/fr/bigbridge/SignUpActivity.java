1ef9346103efe128cc454a7e225285a932505a5f
CHEN Datao <chendatao1126@gmail.com>
2019-01-26 15:49:21 +0100

    Create SignUpActivity.java

---
+++ app/src/main/java/bigbridge/mbds/unice/fr/bigbridge/SignUpActivity.java
@@ -0,0 +1,13 @@
+package bigbridge.mbds.unice.fr.bigbridge;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
