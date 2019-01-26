01bb0a469407fa9b9a22e7687c51860007cafdc0
CHEN Datao <chendatao1126@gmail.com>
2019-01-26 15:49:06 +0100

    Create Auth.java

---
+++ app/src/main/java/bigbridge/mbds/unice/fr/bigbridge/api/model/Auth.java
@@ -0,0 +1,37 @@
+package bigbridge.mbds.unice.fr.bigbridge.api.model;
+
+public class Auth {
+    private String email;
+    private String username;
+    private String password;
+
+    public Auth(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
