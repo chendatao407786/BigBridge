dc5cd7e56f34a9383d50ff5871e89c8ea4e45212
CHEN Datao <chendatao1126@gmail.com>
2019-01-27 19:46:47 +0100

    Create PreferencesManager.java

---
+++ app/src/main/java/bigbridge/mbds/unice/fr/bigbridge/util/PreferencesManager.java
@@ -0,0 +1,60 @@
+package bigbridge.mbds.unice.fr.bigbridge.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PreferencesManager {
+    private static PreferencesManager preferencesManager;
+    private SharedPreferences sharedPreferences;
+
+    private static final String PREFS_NAME = "myPreferences";
+    private static final String PREFS_USERNAME = "prefs_username";
+    private static final String PREFS_EMAIL = "prefs_email";
+    private static final String PREFS_PASSWORD = "prefs_password";
+    private static final String PREFS_TOKEN = "prefs_token";
+
+    public static PreferencesManager getInstance(Context context) {
+
+        //test and return instance class
+        if(preferencesManager == null){
+            preferencesManager = new PreferencesManager(context);
+        }
+        return preferencesManager;
+    }
+
+    private PreferencesManager(Context context) {
+        sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+    }
+
+    public void saveUsername(String username){
+        sharedPreferences.edit().putString(PREFS_USERNAME, username).apply();
+    }
+
+    public String loadUsername(){
+        return sharedPreferences.getString(PREFS_USERNAME, null);
+    }
+
+    public void saveEmail(String email){
+        sharedPreferences.edit().putString(PREFS_EMAIL, email).apply();
+    }
+    public String loadEmail(){
+        return sharedPreferences.getString(PREFS_EMAIL,null);
+    }
+    public void savePwd(String password){
+        sharedPreferences.edit().putString(PREFS_PASSWORD, password).apply();
+    }
+
+    public String loadPwd(){
+        return sharedPreferences.getString(PREFS_PASSWORD, null);
+    }
+
+
+
+    public void saveToken(String accessToken){
+        sharedPreferences.edit().putString(PREFS_TOKEN, accessToken).apply();
+    }
+
+    public String loadToken(){
+        return sharedPreferences.getString(PREFS_TOKEN, null);
+    }
+}
