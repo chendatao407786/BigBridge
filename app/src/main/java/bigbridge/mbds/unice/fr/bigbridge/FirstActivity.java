a2480a981cb0b09831c0e71de0c2c6963ed22bcf
CHEN Datao <chendatao1126@gmail.com>
2019-01-27 20:59:46 +0100

    Change the name of mainActivity to firstActivity

--- app/src/main/java/bigbridge/mbds/unice/fr/bigbridge/FirstActivity.java
+++ app/src/main/java/bigbridge/mbds/unice/fr/bigbridge/FirstActivity.java
@@ -5,17 +5,17 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity {
     Handler mHandle = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first);
         mHandle.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(FirstActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         }, 1000);
