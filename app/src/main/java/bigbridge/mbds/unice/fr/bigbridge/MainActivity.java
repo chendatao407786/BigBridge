c11af29f9426a78787d05dd4d49609a9524f76ac
CHEN Datao <chendatao1126@gmail.com>
2019-01-28 14:19:31 +0100

    Update MainActivity.java

--- app/src/main/java/bigbridge/mbds/unice/fr/bigbridge/MainActivity.java
+++ app/src/main/java/bigbridge/mbds/unice/fr/bigbridge/MainActivity.java
@@ -1,22 +1,51 @@
 package bigbridge.mbds.unice.fr.bigbridge;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import bigbridge.mbds.unice.fr.bigbridge.fragment.AirMonitorFragment;
 import bigbridge.mbds.unice.fr.bigbridge.fragment.MyProfileFragment;
+import bigbridge.mbds.unice.fr.bigbridge.fragment.ResultFragment;
+import bigbridge.mbds.unice.fr.bigbridge.fragment.WellBeingFragment;
 
 public class MainActivity extends AppCompatActivity {
     String username;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Bundle bundle = getIntent().getExtras();
         username = bundle.getString("username");
+        bottomNavigationView = findViewById(R.id.botton_nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.profile:
+                        createProfileFragement();
+                        return true;
+                    case R.id.well_being:
+                        createWellBeingFragment();
+                        return true;
+                    case R.id.pollution:
+                        createAirMonitorFragement();
+                        return true;
+                    case R.id.result:
+                        createResultFragement();
+                        return true;
+                }
+                return false;
+            }
+        });
         createProfileFragement();
     }
 
@@ -25,9 +54,30 @@ public class MainActivity extends AppCompatActivity {
         Bundle bundle = new Bundle();
         bundle.putString("username",username);
         myProfileFragment.setArguments(bundle);
+        createFragement(myProfileFragment);
+    }
+
+    private void createWellBeingFragment(){
+        WellBeingFragment wellBeingFragment = new WellBeingFragment();
+//        Bundle bundle = new Bundle();
+//        bundle.putString("username",username);
+//        wellBeingFragment.setArguments(bundle);
+        createFragement(wellBeingFragment);
+    }
+
+    private void createAirMonitorFragement(){
+        AirMonitorFragment airMonitorFragment = new AirMonitorFragment();
+        createFragement(airMonitorFragment);
+    }
+
+    private void createResultFragement(){
+        ResultFragment resultFragment = new ResultFragment();
+        createFragement(resultFragment);
+    }
+    private void createFragement(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.content_main,myProfileFragment);
+        fragmentTransaction.replace(R.id.content_main,fragment);
         fragmentTransaction.commit();
     }
 }
