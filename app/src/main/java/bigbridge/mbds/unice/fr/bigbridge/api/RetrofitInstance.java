e9c2ff8a00a5e4efb635b6bae162b2a446292248
CHEN Datao <chendatao1126@gmail.com>
2019-01-28 14:19:15 +0100

    Update RetrofitInstance.java

--- app/src/main/java/bigbridge/mbds/unice/fr/bigbridge/api/RetrofitInstance.java
+++ app/src/main/java/bigbridge/mbds/unice/fr/bigbridge/api/RetrofitInstance.java
@@ -6,7 +6,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
     private static Retrofit retrofit;
 //    private static final String BASE_URL = "http://10.0.0.2:8080/api/";
-    private static final String BASE_URL = "http://192.168.0.14:8080/api/";
+//    private static final String BASE_URL = "http://192.168.0.14:8080/api/";
+    private static final String BASE_URL = "http://10.154.97.218:52423/api/";
 
     /**
      * Create an instance of Retrofit object
