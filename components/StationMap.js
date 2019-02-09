d9cea55cf8af2d1e1615181035883b4db91ac51d
CHEN Datao <chendatao1126@gmail.com>
2019-02-09 01:31:32 +0100

    Update StationMap.js

--- components/StationMap.js
+++ components/StationMap.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ToastAndroid, Button, PermissionsAndroid, ScrollView,Dimensions,StatusBar } from 'react-native';
-import axios from 'axios';
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions, StatusBar } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import AQIDetails from './AQIDetails';
+import CustomCallout from './CustomCallout';
 
 
 class StationMap extends Component {
@@ -16,6 +16,7 @@ class StationMap extends Component {
         this.getUserLocation = this.getUserLocation.bind(this);
         this.getNearestStation = this.getNearestStation.bind(this);
         this.onPressHandler = this.onPressHandler.bind(this);
+        this.markerColor = this.markerColor.bind(this);
     }
     componentWillMount() {
 
@@ -49,11 +50,6 @@ class StationMap extends Component {
                         }
                     }
                     newStations.push(oneStation);
-                    // newCity.push(station.nlo)
-                    // newMarkers.push(oneMarker);
-                    // this.setState({
-                    // markers: [station.geo, ...this.state.markers],
-                    // })
                 })
                 this.setState({
                     stations: newStations
@@ -73,6 +69,14 @@ class StationMap extends Component {
             })
         }, err => console.log(err))
     }
+
+    createPolluant(name, value, update_time) {
+        return ({
+            name: name,
+            value: value,
+            update: update_time
+        })
+    }
     onPressHandler(id) {
         let url = 'https://api.waqi.info/api/feed/@' + id + '/obs.fr.json'
         console.log(url);
@@ -88,13 +92,43 @@ class StationMap extends Component {
                 console.log(res);
                 let polluants = [];
                 res.iaqi.map(polluant => {
-                    let newPolluant = {
-                        name: polluant.p,
-                        value: polluant.v,
-                        update: polluant.h,
+                    let newPolluant;
+                    switch (polluant.p) {
+                        case 'pm25':
+                            newPolluant = { name: "PM2.5", value: polluant.v, update: polluant.h, unity: 'AQI',srcImage:"pm25" };
+                            break;
+                        case 'pm10':
+                            newPolluant = { name: "PM10", value: polluant.v, update: polluant.h, unity: 'AQI',srcImage:"pm10"  };
+                            break;
+                        case 'o3':
+                            newPolluant = { name: "O3", value: polluant.v, update: polluant.h, unity: 'AQI',srcImage:"o3"  };
+                            break;
+                        case 'no2':
+                            newPolluant = { name: "NO2", value: polluant.v, update: polluant.h, unity: 'AQI',srcImage:"no2"  }
+                            break;
+                        case 'so2':
+                            newPolluant = { name: "SO2", value: polluant.v, update: polluant.h, unity: 'AQI',srcImage:"so2"  }
+                            break;
+                        case 't':
+                            newPolluant = { name: "Temperature", value: polluant.v, update: polluant.h, unity: '°C' ,srcImage:"temperature" }
+                            break;
+                        case 'p':
+                            newPolluant = { name: "Atmos.", value: polluant.v, update: polluant.h, unity: 'MBAR',srcImage:"atmos"  }
+                            break;
+                        case 'h':
+                            newPolluant = { name: "Humidity", value: polluant.v, update: polluant.h, unity: '%',srcImage:"humidity"  }
+                            break;
+                        case 'w':
+                            newPolluant = { name: "Wind", value: polluant.v, update: polluant.h, unity: 'M/S' ,srcImage:"wind" }
+                            break;
+                        case 'co':
+                            newPolluant = { name: "CO", value: polluant.v, update: polluant.h, unity: 'AQI' ,srcImage:"co" }
+                            break;
+                        default:
+                            newPolluant = { name: String(polluant.p).toUpperCase(), value: polluant.v, update: polluant.h, unity: '' }
                     }
                     polluants.push(newPolluant);
-                })
+                });
                 let station = {
                     aqi: res.aqi,
                     time: res.time,
@@ -111,6 +145,25 @@ class StationMap extends Component {
             })
     }
 
+    markerColor = (value) => {
+        let bkcolor;
+        if (value <= 50) {
+            bkcolor = { backgroundColor: '#1A936F' };
+        } else if (value <= 100) {
+            bkcolor = { backgroundColor: '#9C9304' };
+        } else if (value <= 150) {
+            bkcolor = { backgroundColor: '#CC970A' };
+        } else if (value <= 200) {
+            bkcolor = { backgroundColor: '#C0380F' };
+        } else if (value <= 300) {
+            bkcolor = { backgroundColor: '#8217C5' };
+        } else if (value > 300) {
+            bkcolor = { backgroundColor: '#8C0F1E' };
+        } else {
+            bkcolor = { backgroundColor: 'gray' };
+        }
+        return bkcolor;
+    }
 
     render() {
         // let station = (
@@ -134,29 +187,61 @@ class StationMap extends Component {
                         }}>
                         {
                             this.state.stations.map((station) => (
-                                <MapView.Marker
+                                <Marker
                                     id={station.id}
-                                    title={station.name}
                                     coordinate={station.coordinate}
-                                    key={station.id}
-                                    onPress={e => this.onPressHandler(station.id)}>
-                                    <View style={[station.value < 50 ? styles.marker_green : styles.marker_orange, styles.marker]}>
-                                        <Text style={styles.text}>{station.value}</Text>
+                                    // calloutOffset={{ x: -1, y: 1 }}
+                                    // calloutAnchor={{ x: 0.5, y: 0.1 }}
+                                    onPress={e => this.onPressHandler(station.id)}
+                                >
+                                    <View>
+                                        <View style={[this.markerColor(station.value), styles.marker]}>
+                                            <Text style={styles.text}>{station.value}</Text>
+                                        </View>
+                                        <Callout tooltip style={styles.customView}>
+                                            <CustomCallout>
+                                                <Text style={{ fontFamily: 'Roboto-Light', fontSize: 15, textAlign: 'center',fontWeight:"300"}}>{station.name}</Text>
+                                            </CustomCallout>
+                                        </Callout>
                                     </View>
-                                </MapView.Marker>
+                                </Marker>
+                                // <Marker
+                                //     id={station.id}
+                                //     // title={station.name}
+                                //     coordinate={station.coordinate}
+                                //     key={station.id}
+                                //     ref={marker => {
+                                //         this.marker1 = marker;
+                                //       }}
+                                //     onPress={e => this.onPressHandler(station.id)}
+                                //     onCalloutPress={() => {
+                                //         console.log("Marker/onCalloutPress");
+                                //         this.marker1.hideCallout();
+                                //     }}>
+                                //     {/* <Callout tooltip={true} style={{width:20,height:20}}>
+                                //         <View>
+                                //             <Text>
+                                //                 callout text
+                                //             </Text>
+                                //         </View>
+                                //     </Callout> */}
+                                //     <View style={[this.markerColor(station.value), styles.marker]}>
+                                //         <Text style={styles.text}>{station.value}</Text>
+                                //     </View>
+                                // </Marker>
                             ))
                         }
                     </MapView>
                     <View style={styles.detailContainer}>
-                    {this.state.currentStation != null ?
-                        <AQIDetails
-                            name={this.state.currentStation.city}
-                            value={this.state.currentStation.aqi}
-                            aqi={this.state.currentStation.polluants}
-                            time={this.state.currentStation.time} />
-                        :
-                        <Text>Nothing here</Text>}
-                    </View>  
+                        {this.state.currentStation != null ?
+                            <AQIDetails
+                                name={this.state.currentStation.city}
+                                value={this.state.currentStation.aqi}
+                                aqi={this.state.currentStation.polluants}
+                                time={this.state.currentStation.time} />
+                            :
+                            <Text>Nothing here</Text>}
+                    </View>
                     {/* <View style={styles.tab}></View> */}
                 </View>
             )
@@ -167,18 +252,27 @@ class StationMap extends Component {
 const styles = StyleSheet.create({
     mainContainer: {
         width: '100%',
-        flex:1
+        flex: 1
+    },
+    customView: {
+        top:-5
+        // width: 140,
+        // height: 100
     },
     map: {
         width: '100%',
-        height: 400
+        height: 300,
+        shadowOffset: { width: 5, height: 5 },
+        shadowOpacity: 0.5,
+        shadowRadius: 5
     },
-    detailContainer:{
+    detailContainer: {
         // backgroundColor:'steelblue',
         // height:100
-        height:(Dimensions.get('window').height-400-56-StatusBar.currentHeight)
+        // backgroundColor:'#DADADA',
+        height: (Dimensions.get('window').height - 300 - 56 - StatusBar.currentHeight)
     },
-    
+
     marker: {
         width: 20,
         height: 20,
@@ -194,7 +288,8 @@ const styles = StyleSheet.create({
         color: '#FFF',
         fontSize: 10,
         lineHeight: 20,
-        textAlign: 'center'
+        textAlign: 'center',
+        fontFamily: 'Roboto-Light'
     }
 })
 
