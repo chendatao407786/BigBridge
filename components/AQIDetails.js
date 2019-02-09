1dee38d49f53e2b8ca49fd32eb4c3a6d09c10012
CHEN Datao <chendatao1126@gmail.com>
2019-02-09 17:54:49 +0100

    Update AQIDetails.js

--- components/AQIDetails.js
+++ components/AQIDetails.js
@@ -12,39 +12,49 @@ class AQIDetails extends Component {
         let background;
         if (value < 50) {
             // green
-            color = "green"
+            color = "#1b5e20"
             level = "Good"
-            background = ['#114B5F', '#1A936F', '#88D498']
+            background = ['#1b5e20', '#2e7d32', '#388e3c']
         } else if (value < 100) {
             // yellow
-            color = "#EFC916"
+            color = "#ffb300"
             level = "Moderate"
-            background = ['#938A04', '#9C9304', '#B6AC04']
+            background = ['#ffb300', '#fbc02d', '#fdd835']
         } else if (value < 150) {
             // orange
-            color = "orange"
+            color = "ff8f00"
             level = "Unhealthy for Sensitive Groups"
-            background = ['#AE8005', '#CC970A', '#DCA718']
+            background = ['#ff8f00', '#ffa000', '#ffb300']
         } else if (value < 200) {
             // red
-            color = "red"
+            color = "c62828"
             level = "Unhealthy"
-            background = ['#A92E09', '#C0380F', '#DA4518']
+            background = ['#c62828', '#d32f2f', '#e53935']
         } else if (value < 300) {
             // purple
-            color = "purple"
+            color = "6a1b9a"
             level = "Very Unhealthy"
-            background = ['#56048A', '#8217C5', '#9230D0']
+            background = ['#6a1b9a', '#7b1fa2', '#8e24aa']
         } else {
             // dark red
-            color = "#6E060C"
+            color = "#3e2723"
             level = "Hazardous"
-            background = ['#7F0513', '#8C0F1E', '#AC1E2F']
+            background = ['#3e2723', '#4e342e', '#5d4037']
         }
         let pollution = this.props.aqi.map((polluant, index) => {
             return (
-                <LinearGradient colors={background} style={[styles.polluantBox, { shadowColor: background[1] }]} key={index}>
-                    <View style={{flexDirection:"row"}}>
+                <LinearGradient colors={background}
+                    style={[styles.polluantBox,
+                    {
+                        shadowOffset: {
+                            width: 3,
+                            height: 3
+                        },
+                        // shadowColor:'black',
+                        elevation: 3,
+                        backgroundColor: background[0]
+                    }]} key={index}>
+                    <View style={{ flexDirection: "row" }}>
                         <Text style={{ fontSize: 15, fontWeight: '100', color: '#FFF', fontFamily: 'Roboto-Light' }}>{polluant.name}</Text>
                     </View>
                     <View style={{ flexDirection: "row", alignItems: 'stretch', marginTop: 10 }}>
@@ -52,25 +62,25 @@ class AQIDetails extends Component {
                         <Text style={{ fontSize: 15, height: 15, fontWeight: '100', fontFamily: 'Roboto-Light', color: '#FFF', marginLeft: 5, textAlignVertical: 'bottom' }}>{polluant.unity}</Text>
                     </View>
                     <Text style={{ fontSize: 15, height: 15, fontWeight: '100', fontFamily: 'Roboto-Light', color: '#FFF', textAlignVertical: 'bottom' }}>Today : {polluant.value[1]} ~ {polluant.value[2]}</Text>
-                    <View style={{width:'100%',flex:1,justifyContent:"center"}}>
-                        <Image 
-                                source={{ uri: polluant.srcImage }} 
-                                style={{ height: 50,width:'100%' }} 
-                                resizeMode={"contain"} />
-                    </View> 
+                    <View style={{ width: '100%', flex: 1, justifyContent: "center" }}>
+                        <Image
+                            source={{ uri: polluant.srcImage }}
+                            style={{ height: 50, width: '100%' }}
+                            resizeMode={"contain"} />
+                    </View>
                 </LinearGradient>
             )
         })
         return (
             <View>
-                    <View style={{ flexDirection: "column", justifyContent: 'center',marginLeft: 15,marginRight: 15}}> 
-                        <Text style={[styles.aqiStyle, { color: background[0] }]}>{this.props.value}</Text>
-                        <Text style={{ color: background[0], fontSize: 20, fontFamily: 'Roboto-Light', fontWeight: '100' }}>{level}</Text>
-                        <View style={{flexDirection:"row",height:15,width:'100%',alignItems:"flex-end"}}>
-                            <Image source={{uri:'update'}} style={{height:15,width:15}} resizeMode={"contain"}/>
-                            <Text style={{fontFamily:'Roboto-Light',fontSize:10}}>last update: {this.props.time.utc.s}</Text>
-                         </View>
+                <View style={{ flexDirection: "column", justifyContent: 'center', marginLeft: 15, marginRight: 15 }}>
+                    <Text style={[styles.aqiStyle, { color: background[0] }]}>{this.props.value}</Text>
+                    <Text style={{ color: background[0], fontSize: 20, fontFamily: 'Roboto-Light', fontWeight: '100' }}>{level}</Text>
+                    <View style={{ flexDirection: "row", height: 15, width: '100%', alignItems: "flex-end" }}>
+                        <Image source={{ uri: 'update' }} style={{ height: 15, width: 15 }} resizeMode={"contain"} />
+                        <Text style={{ fontFamily: 'Roboto-Light', fontSize: 10 }}>last update: {this.props.time.utc.s}</Text>
                     </View>
+                </View>
                 <ScrollView style={styles.detailContainer} horizontal={true}>
                     <View style={styles.poluantList}>
                         {pollution}
