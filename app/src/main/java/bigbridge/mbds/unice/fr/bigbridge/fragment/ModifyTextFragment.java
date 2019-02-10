bfb7ca310236f62a099c5fa745c4404282cd9f33
CHEN Datao <chendatao1126@gmail.com>
2019-02-10 02:00:19 +0100

    Update ModifyTextFragment.java

--- app/src/main/java/bigbridge/mbds/unice/fr/bigbridge/fragment/ModifyTextFragment.java
+++ app/src/main/java/bigbridge/mbds/unice/fr/bigbridge/fragment/ModifyTextFragment.java
@@ -3,9 +3,12 @@ package bigbridge.mbds.unice.fr.bigbridge.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import bigbridge.mbds.unice.fr.bigbridge.R;
 
@@ -13,7 +16,7 @@ import bigbridge.mbds.unice.fr.bigbridge.R;
  * A simple {@link Fragment} subclass.
  */
 public class ModifyTextFragment extends ModifierFragment {
-
+    protected TextView mTextView;
     public ModifyTextFragment() {
     }
 
@@ -23,5 +26,26 @@ public class ModifyTextFragment extends ModifierFragment {
         initToolBar(view,container);
         initModifyContent(view);
         return view;
+    }
+
+    private void initModifyContent( View view){
+        mTextView = view.findViewById(R.id.modify_content);
+        mTextView.setText(mValue);
+        mTextView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mValue = mTextView.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
